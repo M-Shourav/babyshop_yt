e7913cdefdb83c15d05a1339e69b313de6ae1229
M-Shourav <muhammadshourav625@gmail.com>
2025-07-30 01:18:37 +0600

    admin panel setup

--- admin/components/ui/commons/Sidebar.tsx
+++ admin/components/ui/commons/Sidebar.tsx
@@ -1,24 +1,18 @@
 "use client";
 import Link from "next/link";
-import { Home, Users, Package } from "lucide-react";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "../command";
-import { Button } from "../button";
+import { Users, Package, House, ListOrdered } from "lucide-react";
 import axios from "axios";
 import { serverUrl } from "@/config";
 import toast from "react-hot-toast";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { User } from "@/types/userType";
 
 const navItems = [
   {
-    label: "Dashboard",
+    label: "Home",
     href: "/",
-    icon: <Home className="w-4 h-4" />,
+    icon: <House className="w-4 h-4" />,
   },
   { label: "Users", href: "user", icon: <Users className="w-4 h-4" /> },
   {
@@ -26,9 +20,31 @@ const navItems = [
     href: "products",
     icon: <Package className="w-4 h-4" />,
   },
+  {
+    label: "Orders",
+    href: "orders",
+    icon: <ListOrdered className="w-4 h-4" />,
+  },
 ];
 
 const Sidebar = () => {
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const getSingleUser = async () => {
+      try {
+        const res = await axios.get(`${serverUrl}/api/auth/singleProfile`, {
+          withCredentials: true,
+        });
+        const data = res?.data;
+        if (data?.success) {
+          setUser(data?.user);
+        }
+      } catch (error) {
+        console.error("single user error:", error);
+      }
+    };
+    getSingleUser();
+  }, []);
   const handleLogout = async () => {
     try {
       const res = await axios.post(
@@ -46,30 +62,31 @@ const Sidebar = () => {
     }
   };
   return (
-    <aside className="w-64 bg-gray-100 h-screen p-2 border-r hidden md:block">
-      <Command className="h-full w-full bg-transparent">
-        <CommandInput placeholder="Type a command or search..." />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup>
-            <CommandItem>
-              <nav className="space-y-2">
-                {navItems.map((item) => (
-                  <Link
-                    key={item.href}
-                    href={item.href}
-                    className="flex items-center gap-2 text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md hover:bg-blue-50 transition isActive"
-                  >
-                    {item.icon}
-                    <span>{item.label}</span>
-                  </Link>
-                ))}
-              </nav>
-            </CommandItem>
-          </CommandGroup>
-        </CommandList>
-        <Button onClick={handleLogout}>Logout</Button>
-      </Command>
+    <aside className="w-36 bg-gray-100 h-screen p-2 border-r hidden md:block">
+      <h2 className="text-xl font-bold mb-6">logo</h2>
+      <nav className="space-y-2">
+        {navItems.map((item) => (
+          <Link
+            key={item.href}
+            href={item.href}
+            className="flex items-center gap-2 text-gray-700 hover:text-blue-600 px-2  rounded-md hover:bg-blue-50 transition"
+          >
+            {item.icon}
+            <span>{item.label}</span>
+          </Link>
+        ))}
+      </nav>
+      <div className="mt-20 ml-4 w-8 h-8 flex items-center justify-center ring-1 rounded-full">
+        {user?.avatar && (
+          <Image
+            src={user?.avatar.url}
+            alt={user?.avatar.public_id}
+            width={20}
+            height={20}
+            className="w-7 h-7 rounded-full flex items-center justify-center"
+          />
+        )}
+      </div>
     </aside>
   );
 };
--- admin/components/ui/commons/Topbar.tsx
+++ admin/components/ui/commons/Topbar.tsx
@@ -5,14 +5,9 @@ import { User } from "@/types/userType";
 import axios from "axios";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import { Popover, PopoverContent, PopoverTrigger } from "../popover";
-import { Command, CommandGroup, CommandItem, CommandList } from "../command";
-import Link from "next/link";
 
 const Topbar = () => {
   const [user, setUser] = useState<User | null>(null);
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
   useEffect(() => {
     const getSingleUser = async () => {
       try {
@@ -33,33 +28,17 @@ const Topbar = () => {
     <div className="h-14 flex items-center justify-between px-4 border-b shadow-sm bg-white">
       <div className="font-semibold text-lg">Admin Dashboard</div>
       <div className=" flex items-center gap-2">
-        <p className="text-sm font-semibold">Welcome {user?.name}</p>
-
-        <Popover open={open} onOpenChange={setOpen}>
-          <PopoverTrigger asChild>
-            {user?.avatar.url && (
-              <Image
-                src={user?.avatar.url}
-                alt={user?.avatar.public_id}
-                width={20}
-                height={20}
-                loading="lazy"
-                className="w-10 h-10 rounded-full object-fill"
-              />
-            )}
-          </PopoverTrigger>
-          <PopoverContent className="w-[200px] ">
-            <Command>
-              <CommandList>
-                <CommandGroup>
-                  <CommandItem>
-                    <Link href={"/user"}>Profile</Link>
-                  </CommandItem>
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
+        <p className="text-xs font-semibold">Welcome {user?.name}</p>
+        {user?.avatar.url && (
+          <Image
+            src={user?.avatar.url}
+            alt={user?.avatar.public_id}
+            width={20}
+            height={20}
+            loading="lazy"
+            className="w-10 h-10 rounded-full object-fill cursor-pointer"
+          />
+        )}
       </div>
     </div>
   );
