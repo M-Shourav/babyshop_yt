a137608c341350afdbdffc2aa97ed0d6a345621f
M-Shourav <muhammadshourav625@gmail.com>
2025-07-30 23:32:23 +0600

    user chart added

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,9 +1,14 @@
+import AdminChart from "@/components/ui/commons/AdminChart";
+import CategoryChart from "@/components/ui/commons/CategoryChart";
 import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h3>Welcome admin panel</h3>
+    <div className="flex flex-col gap-4">
+      <div className=" grid grid-cols-1 md:grid-cols-2 gap-5">
+        <CategoryChart />
+        <AdminChart />
+      </div>
     </div>
   );
 };
---
+++ admin/components/ui/commons/AdminChart.tsx
@@ -0,0 +1,91 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Card, CardContent, CardFooter, CardHeader } from "../card";
+import { ResponsiveContainer, PieChart, Pie, Cell } from "recharts";
+import axios from "axios";
+import { serverUrl } from "@/config";
+
+const customLabel = ({ name, percent }: any) => {
+  const label = `${(percent * 100).toFixed(0)}% ${name}`;
+  return label;
+};
+
+const AdminChart = () => {
+  const [data, setData] = useState<{ name: string; value: number }[]>([]);
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await axios.get(`${serverUrl}/api/auth/profile`, {
+          withCredentials: true,
+        });
+        const users = res.data.user;
+
+        const adminCount = users.filter(
+          (user: any) => user.role === "admin"
+        ).length;
+
+        const userCount = users.filter(
+          (user: any) => user.role === "user"
+        ).length;
+        const deliveryCount = users.filter(
+          (user: any) => user.role === "deliveryman"
+        ).length;
+        const chartData = [];
+        if (adminCount > 0)
+          chartData.push({ name: "Admin", value: adminCount });
+        if (userCount > 0) chartData.push({ name: "user", value: userCount });
+        if (deliveryCount > 0)
+          chartData.push({ name: "deliveryman", value: deliveryCount });
+        setData(chartData);
+      } catch (error) {
+        console.log("Failed to fetch usersList:", error);
+      }
+    };
+
+    getUser();
+  }, []);
+
+  const colors = ["#1A2A80", "#954C2E", "#00809D"];
+
+  return (
+    <Card>
+      <CardHeader>
+        <p className="text-sm font-semibold">User Roles Distribution</p>
+      </CardHeader>
+      <CardContent className="w-[100%] h-[300px]">
+        <ResponsiveContainer>
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="value"
+              cx="50%"
+              cy="50%"
+              label={customLabel}
+              outerRadius={100}
+            >
+              {data.map((_, index) => (
+                <Cell
+                  type="monotone"
+                  key={index}
+                  fill={colors[index % colors.length]}
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </CardContent>
+      <CardFooter className="flex items-center justify-center gap-4">
+        <div className="flex items-center gap-x-2">
+          <span className="w-4 h-4 bg-[#954C2E] inline-flex" />
+          <p>User</p>
+        </div>
+        <div className="flex items-center gap-x-2">
+          <span className="w-4 h-4 bg-[#1A2A80] inline-flex" />
+          <p>Admin</p>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default AdminChart;
