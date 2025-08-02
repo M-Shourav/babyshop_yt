50f4c571c6b8ff47fcf61a63d93acfcece6899c4
M-Shourav <muhammadshourav625@gmail.com>
2025-08-02 20:10:18 +0600

    pagination added

--- admin/components/commons/UserData.tsx
+++ admin/components/commons/UserData.tsx
@@ -32,10 +32,34 @@ import {
   AlertDialogTrigger,
 } from "../ui/alert-dialog";
 import toast from "react-hot-toast";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "../ui/pagination";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 const UserData = () => {
   const [userData, setUserData] = useState<UserType[]>([]);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemPerPage, setItemPerPage] = useState(4);
+
+  const paginateData = userData?.slice(
+    (currentPage - 1) * itemPerPage,
+    currentPage * itemPerPage
+  );
+  const totalPage = Math.ceil(userData.length / itemPerPage);
+
   const fetchData = async () => {
     setLoading(true);
     try {
@@ -85,8 +109,44 @@ const UserData = () => {
       ) : (
         <Card>
           <CardContent>
+            {/* user per-page filter */}
+            <div className="flex items-center justify-between mb-4">
+              <div>
+                <h1>filter data</h1>
+              </div>
+              <div className="flex items-center space-x-2">
+                <span className="text-sm font-medium text-muted-foreground">
+                  users per page
+                </span>
+                <Select
+                  onValueChange={(value) => {
+                    setItemPerPage(Number(value));
+                  }}
+                >
+                  <SelectTrigger className="w-20 py-4 focus-visible:ring-1">
+                    <SelectValue placeholder={`${itemPerPage}`} />
+                  </SelectTrigger>
+                  <SelectContent className="min-w-[20px]">
+                    <SelectItem value="4" className=" cursor-pointer">
+                      4
+                    </SelectItem>
+                    <SelectItem value="10" className=" cursor-pointer">
+                      10
+                    </SelectItem>
+                    <SelectItem value="30" className=" cursor-pointer">
+                      30
+                    </SelectItem>
+                    <SelectItem value="50" className=" cursor-pointer">
+                      50
+                    </SelectItem>
+                    <SelectItem value="100" className=" cursor-pointer">
+                      100
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
             <Table>
-              <TableCaption>A list of recent user</TableCaption>
               <TableHeader className="w-full">
                 <TableRow>
                   <TableHead className="hidden md:table-cell">List</TableHead>
@@ -98,7 +158,7 @@ const UserData = () => {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {userData?.map((item, index) => (
+                {paginateData?.map((item, index) => (
                   <TableRow key={item?._id}>
                     <TableCell className="hidden md:table-cell">
                       {index + 1}
@@ -185,6 +245,64 @@ const UserData = () => {
                 ))}
               </TableBody>
             </Table>
+            {/* pagination Data */}
+            <div className="w-full max-w-screen-sm mt-10 flex items-center justify-between">
+              <div>
+                <p className="text-sm font-semibold text-muted-foreground">
+                  Total Pages: {totalPage}
+                </p>
+              </div>
+              <div>
+                {totalPage > 1 && (
+                  <Pagination>
+                    <PaginationContent>
+                      <PaginationItem>
+                        <PaginationPrevious
+                          onClick={() =>
+                            setCurrentPage((prev) => Math.max(prev - 1, 1))
+                          }
+                          className={`${
+                            currentPage === 1
+                              ? " pointer-events-none opacity-50"
+                              : " cursor-pointer"
+                          }`}
+                        />
+                      </PaginationItem>
+                      {[...Array(totalPage)].map((_, i) => (
+                        <PaginationItem key={i}>
+                          <Button
+                            size="icon"
+                            onClick={() => setCurrentPage(i + 1)}
+                            className={`px-3 py-1 text-xs border border-gray-600 w-7 h-7 flex items-center justify-center ${
+                              currentPage === i + 1
+                                ? "bg-black text-white"
+                                : "bg-white text-black hover:text-white duration-200"
+                            } `}
+                          >
+                            {i + 1}
+                          </Button>
+                        </PaginationItem>
+                      ))}
+
+                      <PaginationItem>
+                        <PaginationNext
+                          onClick={() =>
+                            setCurrentPage((prev) =>
+                              Math.min(prev + 1, totalPage)
+                            )
+                          }
+                          className={`${
+                            currentPage === totalPage
+                              ? "pointer-events-none opacity-50"
+                              : "cursor-pointer"
+                          }`}
+                        />
+                      </PaginationItem>
+                    </PaginationContent>
+                  </Pagination>
+                )}
+              </div>
+            </div>
           </CardContent>
         </Card>
       )}
