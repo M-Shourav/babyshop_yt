3e18a575a1caebb4b027be65cdc512a0c0743533
M-Shourav <muhammadshourav625@gmail.com>
2025-08-08 21:59:08 +0600

    product filter added

--- admin/components/commons/product/ProductData.tsx
+++ admin/components/commons/product/ProductData.tsx
@@ -22,7 +22,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import Image from "next/image";
-import { Loader2, Trash2 } from "lucide-react";
+import { ListFilter, Loader2, Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   AlertDialog,
@@ -37,10 +37,43 @@ import {
 } from "@/components/ui/alert-dialog";
 import EditProduct from "./EditProduct";
 import { EmptyProduct } from "@/assets/images";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 const ProductData = () => {
   const [product, setProduct] = useState<ProductType[]>([]);
   const [loading, setLoading] = useState(false);
+  const [searchPd, setSearchPd] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productPerPage, setProductPerPage] = useState(10);
+
+  // filter product for search
+  const filterProduct = product.filter((product) => {
+    const search = searchPd.toLowerCase();
+    return product.title?.toLowerCase().includes(search);
+  });
+
+  // paginate product per page
+  const paginate = filterProduct?.slice(
+    (currentPage - 1) * productPerPage,
+    currentPage * productPerPage
+  );
+  // total page
+  const totalPage = Math.ceil(filterProduct.length / productPerPage);
+
   const getProduct = async () => {
     try {
       const res = await axios.get(`${serverUrl}/api/product/all-product`, {
@@ -90,9 +123,51 @@ const ProductData = () => {
     <Card>
       {product.length ? (
         <>
-          <CardHeader>
-            <CardTitle>Product page</CardTitle>
-            <CardDescription>All products</CardDescription>
+          <CardHeader className="flex items-center justify-between gap-4">
+            <CardTitle>
+              <div className="flex items-center gap-1 max-w-xs">
+                <div className="w-20 h-9 hidden md:inline-flex items-center justify-center gap-1 border rounded-sm">
+                  <ListFilter size={15} className="font-semibold" />
+                  <span className="text-sm font-semibold">Filter</span>
+                </div>
+                <Input
+                  type="text"
+                  placeholder="Search product with title..."
+                  className=" placeholder:text-sm placeholder:font-normal focus-visible:ring-0 w-[200px]"
+                  value={searchPd}
+                  onChange={(e) => {
+                    setSearchPd(e.target.value);
+                  }}
+                />
+              </div>
+            </CardTitle>
+            <CardDescription className="hidden md:inline-flex items-center gap-2">
+              <p>Product per page</p>
+              <Select
+                onValueChange={(value) => {
+                  setProductPerPage(Number(value));
+                  setCurrentPage(1);
+                }}
+              >
+                <SelectTrigger className=" focus-visible:ring-0">
+                  <SelectValue placeholder={productPerPage} />
+                </SelectTrigger>
+                <SelectContent className="min-w-[20px]">
+                  <SelectItem value="20" className=" cursor-pointer">
+                    20
+                  </SelectItem>
+                  <SelectItem value="50" className=" cursor-pointer">
+                    50
+                  </SelectItem>
+                  <SelectItem value="70" className=" cursor-pointer">
+                    70
+                  </SelectItem>
+                  <SelectItem value="100" className=" cursor-pointer">
+                    100
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </CardDescription>
             <CardAction>
               <Button>
                 <Link href={"/products/create-product"}>Add product</Link>
@@ -113,7 +188,7 @@ const ProductData = () => {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {product?.map((product, index) => (
+                {paginate?.map((product, index) => (
                   <TableRow key={product?._id}>
                     <TableCell className="hidden md:table-cell">
                       {index + 1}
@@ -204,6 +279,57 @@ const ProductData = () => {
                 ))}
               </TableBody>
             </Table>
+            <div className="mt-10 flex items-center justify-between">
+              <p className="text-sm w-full flex items-center font-semibold text-muted-foreground">
+                Total Pages:{" "}
+                <span className="ml-2 text-red-500"> {totalPage}</span>
+              </p>
+              <Pagination>
+                <PaginationContent>
+                  <PaginationItem>
+                    <PaginationPrevious
+                      onClick={() => {
+                        setCurrentPage((prev) => Math.max(prev - 1, 1));
+                      }}
+                      className={`${
+                        currentPage === 1
+                          ? " pointer-events-none opacity-50"
+                          : " cursor-pointer"
+                      }`}
+                    />
+                  </PaginationItem>
+                  {[...Array(totalPage)].map((_, i) => (
+                    <PaginationItem key={i}>
+                      <Button
+                        onClick={() => {
+                          setCurrentPage(i + 1);
+                        }}
+                        className={`px-3 py-1 text-xs border border-gray-600 w-7 h-7 flex items-center justify-center ${
+                          currentPage === i + 1
+                            ? "bg-black text-white"
+                            : "bg-white text-black hover:text-white duration-200"
+                        } `}
+                        size="icon"
+                      >
+                        {i + 1}
+                      </Button>
+                    </PaginationItem>
+                  ))}
+                  <PaginationItem>
+                    <PaginationNext
+                      onClick={() => {
+                        setCurrentPage((prev) => Math.min(prev + 1, totalPage));
+                      }}
+                      className={`${
+                        currentPage === totalPage
+                          ? " pointer-events-none opacity-50"
+                          : "cursor-pointer"
+                      }`}
+                    />
+                  </PaginationItem>
+                </PaginationContent>
+              </Pagination>
+            </div>
           </CardContent>
         </>
       ) : (
